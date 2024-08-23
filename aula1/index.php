4919ef52d8711593c4a58133e783fa8c5bd3e1cc
NicolasDiovaniDias <nicolasdiovanioliveira@gmail.com>
2024-08-23 10:04:19 -0300

    Create index.php

---
+++ aula1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+    $cafeManha = array("maçã" , "café" , "queijão" , "uranio");
+    echo "nicolas";
+    echo "<hr>";
+    echo $cafeManha[1];
+    echo "<hr>";
+    echo $cafeManha[2];
+    echo "<hr>";
+    print_r($cafeManha);
+    echo "<hr>";
+    $nicolas = array(
+        "nome" => "nicolas",
+        "idade" => 18,
+        "cidade" => "eldorado",
+        "signo" => "leão"
+    );
+    print_r($nicolas);
+?>
